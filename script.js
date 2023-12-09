2353698db39aae3212218c6db5c8d0a9c4ca4421
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-12-09 06:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function nextPalindrome(num) {
   //your JS code here.
+	function palindrome(number){
+		return  num.toString()===num.toString().split('').reverse().join('');
+		
+	}
+ 
+	while (true) {
+		num++;
+		if(palindrome(num)){
+			alert(num);
+			break;
+		}
+		
+	}
+	
 }
 
 const input = prompt("Enter a palindrome number");
-alert(nextPalindrome(input));
+const inputNumber = parseInt(input);
+if(input>0 && input<100000){
+	nextPalindrome(inputNumber);
+	
+}
+
